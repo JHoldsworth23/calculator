80be903823b8471af9a322bf82f5442102f2baad
JHoldsworth23 <jacobph101@icloud.com>
2023-11-11 22:37:34 +0000

    Add functionality to equal button to split the string to calculate the equation

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const subtract = (numA, numB) => numA - numB;
 const multiply = (numA, numB) => numA * numB;
 const divide = (numA, numB) => numA / numB;
 
-const equation = document.querySelector(".equation")
+const equation = document.querySelector(".equation");
+const previousEquation = document.querySelector(".previous-answer");
 const numberBtns = document.querySelectorAll(".btn");
 numberBtns.forEach(btn => (
     btn.addEventListener('click', () => {
@@ -19,19 +20,27 @@ operatorBtns.forEach(btn => (
 ));
 
 // user input in the calculator
-let firstOperand = 2;
-let operator = "+";
-let secondOperand = 2;
-
-// create an object for operator : function
-const calculation = {
-    "+": add(firstOperand, secondOperand),
-    "-": subtract(firstOperand, secondOperand),
-    "*": multiply(firstOperand, secondOperand),
-    "/": divide(firstOperand, secondOperand)
-};
+let firstOperand = null;
+let operator = "";
+let secondOperand = null;
 
 // create an "operate" function to call the function 
 function operate() {
     return calculation[operator];
 }
+
+const equalBtn = document.querySelector(".answer-btn");
+equalBtn.addEventListener('click', () => {
+    const equationArray = equation.textContent.split(" ");
+    firstOperand = parseFloat(equationArray[0]);
+    secondOperand = parseFloat(equationArray[2]);
+    console.log(typeof firstOperand);
+});
+
+// create an object for operator : function
+const calculation = {
+    "+": add(firstOperand, secondOperand),
+    "-": subtract(firstOperand, secondOperand),
+    "x": multiply(firstOperand, secondOperand),
+    "÷": divide(firstOperand, secondOperand)
+};
