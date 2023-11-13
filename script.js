e701008100a64c34a637a3077946cfc7a68ac155
JHoldsworth23 <jacobph101@icloud.com>
2023-11-13 18:51:38 +0000

    Rearrange the code

--- script.js
+++ script.js
@@ -3,35 +3,36 @@ let secondOperand = '';
 let currentOperator = '';
 let shouldReset = false;
 
-const add = (numA, numB) => numA + numB;
-const subtract = (numA, numB) => numA - numB;
-const multiply = (numA, numB) => numA * numB;
-const divide = (numA, numB) => numA / numB;
-
 const equation = document.querySelector(".equation");
 const answer = document.querySelector(".answer");
 
 const numberBtns = document.querySelectorAll(".btn");
+const operatorBtns = document.querySelectorAll(".operator-btn");
+const equalBtn = document.querySelector(".answer-btn");
+const clearBtn = document.querySelector(".clear-btn");
+
 numberBtns.forEach(btn => (
     btn.addEventListener('click', () => {
         equation.textContent += btn.textContent;
     })
 ));
 
-const operatorBtns = document.querySelectorAll(".operator-btn");
 operatorBtns.forEach(btn => (
     btn.addEventListener('click', () => {
         equation.textContent += ` ${btn.textContent} `;
     })
 ));
 
-const equalBtn = document.querySelector(".answer-btn");
 equalBtn.addEventListener('click', calculate);
 
-const clearBtn = document.querySelector(".clear-btn");
 clearBtn.addEventListener('click', () => {
     equation.textContent = '';
 })
+
+const add = (numA, numB) => numA + numB;
+const subtract = (numA, numB) => numA - numB;
+const multiply = (numA, numB) => numA * numB;
+const divide = (numA, numB) => numA / numB;
 
 function operate() {
     const calculation = {
@@ -49,7 +50,7 @@ function calculate() {
 
     firstOperand = parseFloat(equationArray[0]);
     secondOperand = parseFloat(equationArray[2]);
-    currentOperator = equationArray[1]
+    currentOperator = equationArray[1];
 
     const equationArray = equation.textContent.split(" ");
     answer.textContent = `${operate()}`;
